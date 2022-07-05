6be1cb86af5b43616eab5d417d766cb12e748e88
wangtianyue <823903677@qq.com>
2022-07-05 09:47:56 +0800

    tijiao

---
+++ train.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from onmt.bin.train import main
+import torch
+
+if __name__ == "__main__":
+    # print(torch.cuda.is_available())
+    # print(torch.__version__)
+    main()
